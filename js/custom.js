437955c4f44bff8dfa0f87069984aa4edd66efb1
KimByungHun <k01087831451@gmail.com>
2021-10-05 16:22:55 +0900

    [update]공통함수 분리

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,19 @@
 var num = 0;
 var timer;
 
-timer = setInterval(function(){
+timer = setInterval(move, 50);
 
+//list박스 마우스 호버하면
+$(".listBox").on("mouseenter",function(){
+    clearInterval(timer);
+});
+$(".listBox").on("mouseleave",function(){
+    timer = setInterval(move,50);
+})
+
+
+//공통함수 분리
+function move(){
     if(num <= -120){
         num = 0;ß
         $(".list li").first().appendTo(".list");
@@ -11,25 +22,4 @@ timer = setInterval(function(){
         num -= 2;
     }
     $(".list").css({left : num});
-    console.log(num);
-},50);
-
-//list박스 마우스 호버하면
-$(".listBox").on("mouseenter",function(){
-    clearInterval(timer);
-});
-$(".listBox").on("mouseleave",function(){
-    timer = setInterval(function(){
-
-        if(num <= -120){
-            num = 0;
-            $(".list li").first().appendTo(".list");
-    
-        }else{
-            num -= 2;
-        }
-        $(".list").css({left : num});
-        console.log(num);
-    },50);
-})
-
+}
