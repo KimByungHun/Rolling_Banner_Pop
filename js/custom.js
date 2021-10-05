4e4af4e039f0bbaa57096095ee4646bec1f170ff
KimByungHun <k01087831451@gmail.com>
2021-10-05 17:28:51 +0900

    [update] 동적 레이어팝업 생성

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 var num = 0;
 var timer;
 
-timer = setInterval(move, 50);
+timer = setInterval(move, 20);
 
 //list박스 마우스 호버하면
 $(".listBox").on("mouseenter",function(){
@@ -11,11 +11,25 @@ $(".listBox").on("mouseleave",function(){
     timer = setInterval(move,50);
 })
 
+$(".list li").on("click", function(e){
+    e.preventDefault();
 
-//공통함수 분리
+    $("body")
+        .append(
+            $("<aside class='pop'>")
+                .append(
+                    $("<div class='con'>"),
+                    $("<span class='btnClose'>").text("close")
+                )
+                .fadeIn(500)
+        )
+});
+
+
+//setinterval로 공통함수 분리
 function move(){
-    if(num <= -120){
-        num = 0;ß
+    if(num <= -240){
+        num = 0;
         $(".list li").first().appendTo(".list");
 
     }else{
